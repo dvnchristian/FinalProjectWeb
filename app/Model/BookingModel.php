190f3f8bf79a09b6069777aba9858f47de1e64e0
Unknown <dvinchristian@gmail.com>
2018-04-03 10:29:59 +0700

    test

--- app/Model/BookingModel.php
+++ app/Model/BookingModel.php
@@ -8,8 +8,23 @@ class BookingModel extends Model
 {
     public $timestamps = false;
     protected $table = "booking";
-    protected $fillable = ['noOfPeople', 'checkInDate', 'checkOutDate','roomQty'];
+    protected $fillable = [
+      'noOfPeople', 'checkInDate', 'checkOutDate','roomQty',
+      'hotelID', 'userID', 'roomID'];
     protected $guarded = [];
 
+    public function HotelModel()
+    {
+      return $this->belongsTo('App\Models\HotelModel');
+    }
 
+    public function UserAccountModelModel()
+    {
+      return $this->belongsTo('App\Models\UserAccountModel');
+    }
+
+    public function RoomTypeModel()
+    {
+      return $this->belongsTo('App\Models\RoomTypeModel');
+    }
 }
--- app/Model/HotelModel.php
+++ app/Model/HotelModel.php
@@ -7,9 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class HotelModel extends Model
 {
     public $timestamps = false;
-    protected $table = "hotel";
-    protected $fillable = ['hotelName', 'hotelLocation', 'hotelAddress','hotelPhone', 'hotelStar'];
+    protected $table = 'hotel';
+    protected $fillable = [
+      'hotelName', 'hotelLocation', 'hotelAddress',
+      'hotelPhone', 'hotelStar', 'roomID'];
     protected $guarded = [];
 
+    public function room()
+    {
+      return $this->belongsTo('App\Models\RoomTypeModel');
+    }
 
 }
