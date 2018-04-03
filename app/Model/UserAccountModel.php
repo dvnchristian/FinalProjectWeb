97bffbf2721e1b1c83ae8efad35c13779e1f810a
Unknown <kevin.djoni@yahoo.com>
2018-04-03 16:48:37 +0700

    Model1

--- app/Model/UserAccountModel.php
+++ app/Model/UserAccountModel.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Model;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
@@ -8,19 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class UserAccountModel extends Authenticatable
 {
     public $timestamps = false;
-<<<<<<< HEAD
     protected $table = "user";
     protected $fillable =
     [
       'email', 'username', 'password',
       'phone'];
     protected $guarded = [];
-=======
-    protected $table = "useraccount";
-    protected $fillable = ['email', 'username', 'password', 'phone', 'fName', 'lName'];
-    protected $guarded = [];
-
-
-
->>>>>>> master
 }
